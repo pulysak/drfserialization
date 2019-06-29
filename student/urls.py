1e3fb06148f26fe54b2ec59f3b5d97048ad52e66
Pavel Lysak <p.lysak@corp.mail.ru>
2019-06-29 18:43:26 +0300

    Added serializer and lsit view

---
+++ student/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('list/', views.StudentListView.as_view(), name='list'),
+]
--- student/views.py
+++ student/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
 
-# Create your views here.
+from student.models import Student
+from student.serializers import StudentSerializer
+
+
+class StudentListView(ListAPIView):
+
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
